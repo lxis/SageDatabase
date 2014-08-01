b971cd702b96cef80cc7e093a7943eb861dde833
lxis <lxis@163.com>
2014-08-01 13:25:09 +0800

    * reflector table

--- SageDatabase/src/com/sage/database/Table.java
+++ SageDatabase/src/com/sage/database/Table.java
@@ -3,10 +3,13 @@ package com.sage.database;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import android.R.integer;
 import android.content.Context;
 
 import com.j256.ormlite.android.apptools.OpenHelperManager;
 import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.stmt.PreparedQuery;
+import com.j256.ormlite.stmt.QueryBuilder;
 
 public class Table<T extends BaseItem>
 {
@@ -14,15 +17,12 @@ public class Table<T extends BaseItem>
 
 	public ObserverbleCollection<T> Items = new ObserverbleCollection<T>();
 
-	private Class<T> itemClass;
-
-	private Context context;
-
-	public Table(Class<T> itemClass, Context context)
+	private Dao<T, Integer> dao;
+	
+	public Table(Class<T> itemClass, Context context) throws SQLException
 	{
-		this.itemClass = itemClass;
-		this.context = context;
-
+		DataHelper dataHelper = OpenHelperManager.getHelper(context, DataHelper.class);
+		dao = dataHelper.getDao(itemClass);
 		Items.addedHandler = new CommonSimpleHandlerGenic<T>()
 		{
 			@Override
@@ -43,28 +43,29 @@ public class Table<T extends BaseItem>
 
 	public void Load() throws SQLException
 	{
-		DataHelper dataHelper = OpenHelperManager.getHelper(context, DataHelper.class);
-		Dao<T, Integer> contactDao = dataHelper.getDao(itemClass);
 		if (Items.size() == 0)
-			Items.addAll(contactDao.queryForAll());
+			Items.addAll(dao.queryForAll());
 		else
 			throw new RuntimeException("重复加载");// 已加载，不能重复加。这个地方的处理没有深入思考，异常也需要细化。
 	}
+	
+//	public void Query(String queryText)
+//	{
+//	
+//	}
 
 	public void Save() throws SQLException
 	{
-		DataHelper dataHelper = OpenHelperManager.getHelper(context, DataHelper.class);
-		Dao<T, Integer> contactDao = dataHelper.getDao(itemClass);
 		for (T item : Items)
 		{
 			if (item.isNew)
-				contactDao.create(item);
-			else if (item.isChanged) contactDao.update(item);
+				dao.create(item);
+			else if (item.isChanged) dao.update(item);
 		}
 
 		for (T item : removedItems)
 		{
-			contactDao.delete(item);
+			dao.delete(item);
 		}
 	}
 
